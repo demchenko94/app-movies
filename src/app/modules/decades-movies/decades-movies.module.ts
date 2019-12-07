f167661a6fde528cfb5079f8b5cea7c00968561c
Demchenko Yevhenii <ydemchenko@entenso.com>
2019-12-07 07:56:41 +0200

    init structure of application

---
+++ src/app/modules/decades-movies/decades-movies.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DecadesMoviesRoutingModule } from './decades-movies-routing.module';
+import { SharedModule } from '@modules/shared/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DecadesMoviesRoutingModule,
+    SharedModule
+  ]
+})
+export class DecadesMoviesModule { }
---
+++ src/app/modules/top-movies/top-movies.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TopMoviesRoutingModule } from './top-movies-routing.module';
+import { SharedModule } from '@modules/shared/shared.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TopMoviesRoutingModule,
+    SharedModule
+  ]
+})
+export class TopMoviesModule { }
